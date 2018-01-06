eb866bcaa09ce9b87ecf48127858c00676adcfa1
ilya <dwanmight@gmail.com>
2018-01-06 11:59:17 +0200

    added macro command

--- Weather-o-Rama-(Command)/src/main/java/com/example/weather_o_rama_command/clients/myClass.java
+++ Weather-o-Rama-(Command)/src/main/java/com/example/weather_o_rama_command/clients/myClass.java
@@ -1,30 +1,40 @@
 package com.example.weather_o_rama_command.clients;
 
+import com.example.weather_o_rama_command.Command;
 import com.example.weather_o_rama_command.commands.GarageDoorCloseCommand;
 import com.example.weather_o_rama_command.commands.GarageDoorOpenCommand;
 import com.example.weather_o_rama_command.commands.LightOffCommand;
 import com.example.weather_o_rama_command.commands.LightOnCommand;
+import com.example.weather_o_rama_command.commands.MacroCommand;
 import com.example.weather_o_rama_command.initiators.RemoteControl;
+import com.example.weather_o_rama_command.initiators.SimpleRemoteControl;
 import com.example.weather_o_rama_command.request_receivers.GarageDoor;
 import com.example.weather_o_rama_command.request_receivers.Light;
 
 public class myClass {
     public static void main(String[] args) {
-//        SimpleRemoteControl simpleRemoteControl = new SimpleRemoteControl();
-//
-//        Light light = new Light();
-//        LightOnCommand lightOnCommand = new LightOnCommand(light);
-//
-//        GarageDoor garageDoor = new GarageDoor();
-//        GarageDoorOpenCommand garageDoorOpenCommand = new GarageDoorOpenCommand(garageDoor);
-//
-//        simpleRemoteControl.setCommand(lightOnCommand);
-//        simpleRemoteControl.buttonWasPressed();
-//        simpleRemoteControl.setCommand(garageDoorOpenCommand);
-//        simpleRemoteControl.buttonWasPressed();
+//        startSimpleSample();
+//        startSample();
+        startMacroSample();
+    }
 
+    private static void startSimpleSample() {
+        SimpleRemoteControl simpleRemoteControl = new SimpleRemoteControl();
+
+        Light light = new Light();
+        LightOnCommand lightOnCommand = new LightOnCommand(light);
+
+        GarageDoor garageDoor = new GarageDoor();
+        GarageDoorOpenCommand garageDoorOpenCommand = new GarageDoorOpenCommand(garageDoor);
+
+        simpleRemoteControl.setCommand(lightOnCommand);
+        simpleRemoteControl.buttonWasPressed();
+        simpleRemoteControl.setCommand(garageDoorOpenCommand);
+        simpleRemoteControl.buttonWasPressed();
+    }
+
+    private static void startSample() {
         RemoteControl remoteControl = new RemoteControl();
-
 
         Light light = new Light();
         GarageDoor garageDoor = new GarageDoor();
@@ -42,6 +52,26 @@ public class myClass {
         remoteControl.undoButtonPushed();
         remoteControl.offButtonWasPressed(1);
         remoteControl.undoButtonPushed();
+    }
 
+    private static void startMacroSample() {
+        RemoteControl macroRemoteControl = new RemoteControl();
+
+        Light light = new Light();
+        GarageDoor garageDoor = new GarageDoor();
+
+        LightOnCommand lightOnCommand = new LightOnCommand(light);
+        LightOffCommand lightOffCommand = new LightOffCommand(light);
+
+        GarageDoorOpenCommand garageDoorOpenCommand = new GarageDoorOpenCommand(garageDoor);
+        GarageDoorCloseCommand garageDoorCloseCommand = new GarageDoorCloseCommand(garageDoor);
+
+        MacroCommand macronOnCommand = new MacroCommand(new Command[]{lightOnCommand, garageDoorOpenCommand});
+        MacroCommand macroOffCommand = new MacroCommand(new Command[]{lightOffCommand, garageDoorCloseCommand});
+
+        macroRemoteControl.setCommand(0, macronOnCommand, macroOffCommand);
+
+        macroRemoteControl.onButtonWasPressed(0);
+        macroRemoteControl.offButtonWasPressed(0);
     }
 }
---
+++ Weather-o-Rama-(Command)/src/main/java/com/example/weather_o_rama_command/commands/MacroCommand.java
@@ -0,0 +1,25 @@
+package com.example.weather_o_rama_command.commands;
+
+import com.example.weather_o_rama_command.Command;
+
+/**
+ * Created by ilya on 06.01.18.
+ */
+
+public class MacroCommand implements Command {
+    private Command[] mCommands;
+
+    public MacroCommand(Command[] commands) {
+        mCommands = commands;
+    }
+
+    @Override public void execute() {
+        for (int i = 0; i < mCommands.length; i++) {
+            mCommands[i].execute();
+        }
+    }
+
+    @Override public void undo() {
+
+    }
+}
